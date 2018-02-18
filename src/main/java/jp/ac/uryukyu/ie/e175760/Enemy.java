02466c7d55fe9412a0c7c4e24b50ae0e28847ce0
yuki yosigai <e175760@ie.u-ryukyu.ac.jp>
2018-02-18 21:31:01 +0900

    report5

--- src/main/java/jp/ac/uryukyu/ie/e175760/Enemy.java
+++ src/main/java/jp/ac/uryukyu/ie/e175760/Enemy.java
@@ -11,12 +11,29 @@ public class Enemy extends LivingThing {
 
     }
 
+    public void attack(LivingThing naltoma) {
+        int damage = (int) (Math.random() * getAttack());
+        int Critical = (int) (Math.random() * 10);
+
+        if (isDead()) {
+            damage = 0;
+            System.out.printf("%sの攻撃！,,,だが、%sは攻撃を回避した！\n", getName(), naltoma.getName());
+        }else if(damage==0){
+            System.out.printf("%sの攻撃！,,,だが、%sは攻撃を回避した！\n", getName(), naltoma.getName());
+        }else if(Critical <= 3){
+            damage = damage*2;
+            System.out.printf("%sの攻撃！会心の一撃！！%sに%dのダメージを与えた！！\n", getName(), naltoma.getName(), damage);
+        }else{
+            System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", getName(), naltoma.getName(), damage);
+        }
+        naltoma.wounded(damage);
+    }
 
     @Override
     public void wounded(int damage){
         setHitPoint(getHitPoint() - damage);
         if( getHitPoint() <= 0 ) {
             setDead(true);
-            System.out.printf("モンスター%sは倒れた。\n", name);
+            System.out.printf("モンスター%sは倒れた。\n", getName());
         }
     }}
--- src/main/java/jp/ac/uryukyu/ie/e175760/Hero.java
+++ src/main/java/jp/ac/uryukyu/ie/e175760/Hero.java
@@ -10,12 +10,28 @@ public class Hero extends LivingThing{
         setDead(false);
 
     }
+    public void attack(LivingThing naltoma) {
+        int damage = (int) (Math.random() * getAttack());
+        int Critical = (int) (Math.random() * 10);
 
+        if (isDead()) {
+            damage = 0;
+            System.out.printf("%sの攻撃！,,,だが、%sは攻撃を回避した！\n", getName(), naltoma.getName());
+        }else if(damage==0){
+            System.out.printf("%sの攻撃！,,,だが、%sは攻撃を回避した！\n", getName(), naltoma.getName());
+        }else if(Critical <= 2){
+            damage = damage*2;
+            System.out.printf("%sの攻撃！会心の一撃！！%sに%dのダメージを与えた！！\n", getName(), naltoma.getName(), damage);
+        }else{
+            System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", getName(), naltoma.getName(), damage);
+        }
+        naltoma.wounded(damage);
+    }
     @Override
     public void wounded(int damage){
         setHitPoint(getHitPoint() - damage);
         if( getHitPoint() <= 0 ) {
             setDead(true);
-            System.out.printf("勇者%sは道半ばで力尽きてしまった。\n", name);
+            System.out.printf("勇者%sは道半ばで力尽きてしまった。\n", getName());
         }
         }}
